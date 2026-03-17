3a4200a6177c930169e00af4e09673ddc14e8f05
Osvaldo Costa <osvaldogcosta90@gmail.com>
2026-03-17 20:04:51 -0300

    Setup google ads (no code yet)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.documentfile)
     implementation(libs.exoplayer)
     implementation("androidx.media:media:1.6.0")
+    implementation("com.google.android.gms:play-services-ads:25.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
